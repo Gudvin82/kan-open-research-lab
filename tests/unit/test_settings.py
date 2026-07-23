41a132691956bed04c190e204e77c953358761be
Gudvin82 <999chekist@gmail.com>
2026-07-24 00:06:20 +0300

    Collect preview static manifest in header test

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -36,11 +36,7 @@ def test_preview_without_database_keeps_liveness_only():
             sys.executable,
             "-c",
             (
-                "import django, tempfile; django.setup(); "
-                "from django.conf import settings; "
-                "settings.STATIC_ROOT=tempfile.mkdtemp(); "
-                "from django.core.management import call_command; "
-                "call_command('collectstatic', verbosity=0, interactive=False); "
+                "import django; django.setup(); "
                 "from django.test import Client; c=Client(); "
                 "assert c.get('/health/live/', "
                 "HTTP_HOST='preview.vercel.app', secure=True).status_code == 200; "
@@ -74,7 +70,11 @@ def test_preview_is_noindex_and_uses_security_headers_without_database():
             sys.executable,
             "-c",
             (
-                "import django; django.setup(); "
+                "import django, tempfile; django.setup(); "
+                "from django.conf import settings; "
+                "settings.STATIC_ROOT=tempfile.mkdtemp(); "
+                "from django.core.management import call_command; "
+                "call_command('collectstatic', verbosity=0, interactive=False); "
                 "from django.test import Client; c=Client(); "
                 "r=c.get('/ru/', HTTP_HOST='preview.vercel.app', "
                 "HTTP_X_FORWARDED_PROTO='https'); "
